aee912b1b959434b4b5a0dec68bf1605c80de3d1
Shankar <shankargit6@gmail.com>
2024-07-17 16:26:59 +0530

    Initial commit

---
+++ companyapi/urls.py
@@ -0,0 +1,9 @@
+# companyapi/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path("api/v1/", include('api.urls')),  # 'api.urls' should match the actual path
+]
